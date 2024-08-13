42fd7b3689709d8fc94f8a96e8f8cf5d66bd0bf2
Iftekhairul Islam <iftekhairul@alphacommerz.com>
2024-08-13 16:34:10 +0600

    feat: filtering more data

--- app/Filters/V1/CustomersFilter.php
+++ app/Filters/V1/CustomersFilter.php
@@ -2,9 +2,10 @@
 
 namespace App\Filters\V1;
 
+use App\Filters\ApiFilter;
 use Illuminate\Http\Request;
 
-class CustomersFilter
+class CustomersFilter extends ApiFilter
 {
     protected $safeParams = [
         'name' => ['eq'],
@@ -19,35 +20,4 @@ class CustomersFilter
     protected $columnMap = [
         'postalCode' => 'postal_code',
     ];
-
-    protected $operatorMap = [
-        'eq' => '=',
-        'lt' => '<',
-        'lte' => '>=',
-        'gt' => '>',
-        'gte' => '>=',
-    ];
-
-    public function transform(Request $request)
-    {
-        $eloQuery = [];
-
-        foreach ($this->safeParams as $param => $operators) {
-            $query = $request->query($param);
-
-            if (!isset($query)) {
-                continue;
-            }
-
-            $column = $this->columnMap[$param] ?? $param;
-
-            foreach ($operators as $operator) {
-                if (isset($query[$operator])) {
-                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
-                }
-            }
-        }
-
-        return $eloQuery;
-    }
 }
---
+++ app/Filters/V1/InvoicesFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters\V1;
+
+use App\Filters\ApiFilter;
+use Illuminate\Http\Request;
+
+class InvoicesFilter extends ApiFilter
+{
+    protected $safeParams = [
+        'customerId' => ['eq'],
+        'amount' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+        'status' => ['eq', 'ne'],
+        'billedDate' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+        'paidDate' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+    ];
+
+    protected $columnMap = [
+        'customerId' => 'postal_code',
+        'billedDate' => 'paid_date',
+        'paidDate' => 'billed_date',
+    ];
+}
